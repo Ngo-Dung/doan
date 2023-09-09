355d057bdc872a75acc78fe25cddf2790721150b
Ngo-Dung <nhochamchoi130401@gmail.com>
2023-09-09 19:02:23 +0700

    baithuchanh2
    bài thực hành buổi 2

---
+++ index.js
@@ -0,0 +1,45 @@
+// console.log("Hello world");
+
+// const myObject = {
+//     2018: "Ha Noi",
+//     2023: "New York",
+// };
+
+// const name= "Dung Ngo";
+
+// let a = 1
+// let b =a
+// a= 2
+// console.log(b);
+
+// let aObject = {
+//     a:1
+// }
+
+// let bObject = aObject;
+// aObject.a =2;
+
+// console.log(bObject.a);
+
+// let a=1
+// let b=2
+// if(a==b) {
+//     console.log(True);
+// } else if (a>b) {
+//     console.log("a lon hon b");
+// } else {
+//     console.log(false);
+// }
+
+// for(let i=0;i<b; i++) {
+//     console.log(i);
+// }
+// let myArray =(1,2,3,4);
+// myArray.forEach((element) =>{
+//     console.log (element);
+// });
+
+// funtion tinhtong(a,b) {
+//     return a+b;
+// }
+
